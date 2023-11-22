e464af0e9f94e3df44fbf2c3498d7712454ca217
IshanGrover2004 <groverishan2004@gmail.com>
2023-11-22 17:34:54 +0530

    feat: Add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/IshanGrover2004/llm-chat.git && cd llm-chat
+
+download_url="https://huggingface.co/rustformers/open-llama-ggml/resolve/main/open_llama_3b-f16.bin"
+
+download_path="./assets/open_llama_3b-f16.bin"
+
+mkdir -p ./assets
+
+wget -O $download_path $download_url
+
+echo "File downloaded successfully and saved to ${download_path}"
